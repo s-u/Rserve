72aeec4fa8e0412665d0ad8b751b16e96b818af6
Simon Urbanek <simon.urbanek@r-project.org>
2003-10-27 15:23:52 +0000

    Minimal R source includes necessary to build Rserve (taken from R 1.8.0 and modified = cut down)

---
+++ src/include/Parse.h
@@ -0,0 +1,15 @@
+#ifndef R_EXT_PARSE_H_
+#define R_EXT_PARSE_H_
+
+typedef enum {
+    PARSE_NULL,
+    PARSE_OK,
+    PARSE_INCOMPLETE,
+    PARSE_ERROR,
+    PARSE_EOF
+} ParseStatus;
+
+SEXP R_ParseVector(SEXP, int, ParseStatus *);
+SEXP R_Parse1Buffer(IoBuffer*, int, ParseStatus *);
+
+#endif
