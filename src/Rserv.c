43b1120e63dd9e8ac59910f3d046ea5dd4708664
Simon Urbanek <simon.urbanek@r-project.org>
2006-11-28 21:59:36 +0000

    Fix issues with including time.h (thanks to BDR)

--- src/Rserv.c
+++ src/Rserv.c
@@ -150,7 +150,16 @@ typedef int socklen_t;
 #include <sisocks.h>
 #include <string.h>
 #ifdef unix
-#include <sys/time.h>
+#if TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# if HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/socket.h>
