71287ca80781d7844dac2c64753a8084290d0c83
Simon Urbanek <simon.urbanek@r-project.org>
2009-01-25 18:01:51 +0000

    cxx demo1: properly delete assigned rexp; clarify mem management comment

--- src/client/cxx/demo1.cc
+++ src/client/cxx/demo1.cc
@@ -44,8 +44,10 @@ int main(int argc, char **argv) {
     double d[6] = { 1.5, 2.4, 5.6, -1.2, 0.6, 1.7 };
     
     // assign the above contents to the variable "a" in R
-    rc->assign("a", new Rdouble(d, 6));
-    
+    Rdouble *rd = new Rdouble(d, 6);
+    rc->assign("a", rd);
+    delete rd;
+
     // create a 2 x 3 matrix named "b" and calculate b * t(b) matrix product
     Rdouble *x = (Rdouble*) rc->eval("b<-matrix(a,2); b%*%t(b)");
     
@@ -67,6 +69,12 @@ int main(int argc, char **argv) {
         delete x;
     }
 
+    // integer constant assignment test
+    int ia[6] = { 1, 4, 6, 3, 5 , 2 };
+    Rinteger *ri = new Rinteger(ia, 6);
+    rc->assign("i", ri);
+    delete ri;
+
     // let's get the whole iris data
     Rvector *iris = (Rvector*) rc->eval("data(iris); iris");
     if (!iris) { cout << "oops! couldn't get iris data\n"; delete rc; return 0; }
@@ -78,8 +86,23 @@ int main(int argc, char **argv) {
     // and print it ...
     { int i=0, ct=sw->length(); while (i<ct) { cout << swd[i++] << " "; }; cout << "\n"; }
     
-    /* One important note about memory management: All necessary memory is allocated by the Rmessage that was implicitly created by "eval". The owner of that message is the main Rexp, that is the one returned by "eval". You must never release any other Rexp than the main one! In addition, any associated Rexp become invalid once you release the main Rexp. Currently there is no deep copy method for Rexp, so you should copy the content in native format if you need it beyond the life of the Rexp. Note that also all pointers such as thos returned by doubleArray() method are invalid as soon as the main Rexp is released.
-        The bottom line: you should release only the iris object, you may not try to delete the sw object. If you add debug info in the destructor, you'll notice that sw gets released automatically. */
+    /* One important note about memory management: All necessary
+       memory is allocated by the Rmessage that was implicitly created
+       by "eval". The owner of that message is the main Rexp, that is
+       the one returned by "eval". You must never release any other
+       Rexp [inside the eval reault] than the main one! In addition,
+       any associated Rexp become invalid once you release the main
+       Rexp. Currently there is no deep copy method for Rexp, so you
+       should copy the content in native format if you need it beyond
+       the life of the Rexp. Note that also all pointers such as thos
+       returned by doubleArray() method are invalid as soon as the
+       main Rexp is released.
+
+       The bottom line: you should release only the iris object, you
+       may not try to delete the sw object. If you add debug info in
+       the destructor, you'll notice that sw gets released
+       automatically. */
+
     delete iris;
     
     // dispose the connection object - this implicitly closes the connection
