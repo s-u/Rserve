1ead5dba4432ed54178c845618011666c2213ae3
Simon Urbanek <simon.urbanek@r-project.org>
2013-08-12 22:18:16 -0400

    prime http_flags with default flags even no upgrade is used

--- src/standalone.c
+++ src/standalone.c
@@ -236,7 +236,7 @@ int main(int argc, char **argv)
 		return ex(1);
 	}
 
-	http_flags = 0;
+	http_flags = global_srv_flags;
 	if (ws_upgrade) {
 		http_flags = global_srv_flags | (enable_ws_qap ? WS_PROT_QAP : 0) | (enable_ws_text ? WS_PROT_TEXT : 0) | (ws_qap_oc ? SRV_QAP_OC : 0);
 		if (http_flags)
