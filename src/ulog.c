09433b9c8894a2cdbf3c74bf3ee3ddd394ce398a
Simon Urbanek <simon.urbanek@r-project.org>
2014-12-25 16:47:18 -0500

    add ulog_reset

--- src/ulog.c
+++ src/ulog.c
@@ -145,9 +145,9 @@ void ulog_add(const char *format, ...) {
 }
 
 void ulog_end() {
-#ifdef RSERV_DEBUG
+#if defined RSERV_DEBUG || defined ULOG_STDERR
     buf[buf_pos] = 0;
-    fprintf(stderr, "ULOG: %s", buf);
+    fprintf(stderr, "ULOG: %s\n", buf);
 #endif
     if (ulog_port) {
 	struct sockaddr_in sa;
@@ -169,6 +169,12 @@ void ulog_end() {
     buf_pos = 0;
 }
 
+void ulog_reset() {
+    if (ulog_sock != -1)
+	close(ulog_sock);
+    ulog_sock = -1;
+}
+
 void ulog(const char *format, ...) {
     va_list(ap);
     va_start(ap, format);
@@ -188,5 +194,6 @@ void ulog_begin() {}
 void ulog_add(const char *format, ...) { }
 void ulog_end() {}
 void ulog(const char *format, ...) { }
+void ulog_reset() {}
 
 #endif
